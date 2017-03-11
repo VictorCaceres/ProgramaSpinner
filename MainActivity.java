07d03892bc156877280c05e3e1e08157a27ce211
VyC27 <vbardalesc12@gmail.com>
2017-03-10 23:27:36 -0600

    Clases de la Actividad Comida

--- MainActivity.java
+++ MainActivity.java
@@ -1,7 +1,10 @@
 package victorcaceres.programaspinner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -33,5 +36,44 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter <String> comidaadaptador = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,datosComida);
         Comida.setAdapter(comidaadaptador);
+
+
+        Fruta.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position)
+                {
+                    case 0:
+                        Intent mz =  new Intent(MainActivity.this,Manzana.class);
+                        startActivity(mz);
+                        break;
+
+                    case 1:
+                        Intent pr =  new Intent(MainActivity.this,Pera.class);
+                        startActivity(pr);
+                        break;
+
+                    case 2:
+                        Intent pl =  new Intent(MainActivity.this,Platano.class);
+                        startActivity(pl);
+                        break;
+
+                    case 3:
+                        Intent uv =  new Intent(MainActivity.this,Uva.class);
+                        startActivity(uv);
+                        break;
+
+                    case 4:
+                        Intent na =  new Intent(MainActivity.this,Naranja.class);
+                        startActivity(na);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
